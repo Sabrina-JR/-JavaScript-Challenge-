7b69e3e1c1397ce4553630e177230c4e1f1c4573
Sabrina <sabrina19960226@gmail.com>
2021-05-11 19:19:28 +0800

    Update 7. canvas

---
+++ 7. canvas/main.js
@@ -0,0 +1,8 @@
+//取DOM
+const Canvas = document.getElementById('canvas-panel');
+const Ctx = Canvas.getContext('2d');
+Canvas.width = innerWidth;
+Canvas.height = innerHeight;
+
+console.log(Canvas)
+
